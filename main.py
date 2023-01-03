ce35f24eb67bddbdf45a55b37710a2a95df31838
joshuaosoro <joshuaosoro@gmail.com>
2023-01-03 16:23:53 +0300

    Headless

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from selenium import webdriver
 import os
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.google.com")
+# headless-mode
+options = Options()
+options.headless = True
+
+def run():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options = options)
+    driver.get("https://www.google.com")
+    print(driver.page_source)
+    #print("hello world")
 
 #chrome_options = webdriver.ChromeOptions()
 #chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
@@ -15,5 +23,6 @@ driver.get("https://www.google.com")
 #chrome_options.add_argument('--no-sandbox')
 #driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chr)
 #driver.get('https://www.google.come')
-print(driver.page_source)
 
+if __name__ == "main":
+    run()
